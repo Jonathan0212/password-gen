bce7f08aff0f3f82974e84d1546e3d1846e89ac7
Your Name <Vaaax@Jon-PC>
2021-12-06 00:52:36 -0600

    wrote out character length prompt function has not been tested

--- Develop/script.js
+++ Develop/script.js
@@ -8,9 +8,28 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
+}
+
+// character length prompt
+function generatePassword () {
+  var characterLength = 0;
+  while ((characterLength < 8, characterLength > 128) || Number.isInteger(characterLength) ===false) {
+characterLength == parseInt(prompt("How many characters would you like your password to have? (8-128)"));
+  }
+
+var upper = false;
+var lower = false;
+var number = false;
+var symbol = false;
+
+while (!upper && !lower && !number && !symbol) {
+
+upper = confirm("Click OK to confirm uppercase characters");
+lower = confirm("Click OK to confirm lowercase characters");
+number = confirm("Click OK to confirm numeral characters");
+symbol = confirm('Click OK to confirm numeral characters ("~!@#$%^&*()-_=+)');
 }
 
 // Add event listener to generate button
