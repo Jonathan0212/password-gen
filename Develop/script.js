56d459ff8a0af3db1860d55c870c798051f1836c
Your Name <Vaaax@Jon-PC>
2021-12-07 22:27:47 -0600

    password is finally generating

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,23 @@
 // Assignment code here
-var charString = ['ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklnmopqrstuvwxyz0123456789"~!@#$%^&*()-_=+'];
+var charString = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklnmopqrstuvwxyz', '0123456789', '"~!@#$%^&*()-_=+'];
 var userInput = [];
 var password = "";
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+ password = generatePassword();
+ console.log('clicked');
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 }
 
 // character length prompt
-function generatePassword () {
+function generatePassword() {
   var characterLength = 0;
-  while ((characterLength < 8, characterLength > 128) || Number.isInteger(characterLength) ===false) {
-characterLength == parseInt(prompt("How many characters would you like your password to have? (8-128)"));
-return password;
-}
+  while ((characterLength < 8 || characterLength > 128) || Number.isInteger(characterLength) === false) {
+    characterLength = parseInt(prompt("How many characters would you like your password to have? (8-128)"));
+};
 
 var upper = false;
 var lower = false;
@@ -50,17 +49,20 @@ if(symbol) {
   userInput.push(charString[3]);
 }
 
-var password='';
+var password="";
 
 //to get array of characters
-userInput=userInput.join('').split('');
+userInput=userInput.join("").split("");
 
-for(var i=0; i < characterLength; i++) {
+for(var i = 0; i < characterLength; i++) {
+
   var index = (Math.floor(Math.random() * userInput.length));
   password= password + userInput [index]
 }
+    
+  return password
 
-};
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # password-generator
+ 
+This application randomly generates a password based on upper case lettering, lower case lettering, numebers, and special characters. Once the user answers the questionare, they are prompted with a password!
+
+
